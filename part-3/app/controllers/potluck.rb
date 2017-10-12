804cc513a1a54278ab21adb2ae102e0f521b9c87
Blaine Anderson <BlaineA97@Gmail.com>
2017-10-12 12:35:44 -0700

    added delete button, deletes potluck from database

--- part-3/app/controllers/potluck.rb
+++ part-3/app/controllers/potluck.rb
@@ -1,16 +1,22 @@
 get '/potluck/new' do
-  @user = User.find(session[:user_id]).username
+  # This applies to the /potluck route
+  @user = User.find(session[:user_id])
   erb :'/potlucks/new'
 end
 
 post '/potluck' do
+  # Could not get user using sessions, removed code from corresponding views to prevent errors, moving on to more releases. To fix this I would go change the user field in the migration of potluck to user_id so i could easily locate the user that way.
   @potluck = Potluck.new(params[:potluck])
   params[:datetime].split(' ')
   date_at = params[:datetime].split(' ')[0]
   time_at = params[:datetime].split(' ')[1]
+  user = params[:user]
   @potluck.date_at = date_at
   @potluck.time_at = time_at
-  if @potluck.name == "" || @potluck.location == "" || @potluck.date_at == "" || @potluck.time_at == "" || @potluck.user_id == nil
+  @potluck.user = user
+  # p "$" * 1000
+  # p params[:user]
+  if @potluck.name == "" || @potluck.location == "" || @potluck.date_at == "" || @potluck.time_at == ""
     @error = "All fields must be filled out to create a potluck."
     erb :'/potlucks/new'
   else
@@ -26,4 +32,17 @@ end
 get '/potluck/:id' do
   @potluck = Potluck.find(params[:id])
   erb :'/potlucks/show'
+end
+
+put '/potluck/:id' do
+  @potluck = Potluck.find(params[:id])
+  @potluck.update(params[:potluck])
+  @potluck.save!
+  erb :"/potluck/show"
+end
+
+delete '/potluck/:id' do
+  Potluck.delete(params[:id])
+  @potlucks = Potluck.all
+  erb :'/index'
 end
--- part-3/db/seeds.rb
+++ part-3/db/seeds.rb
@@ -3,6 +3,8 @@ require 'faker'
 User.create(username: "blaine", email:
 "blaine", password: "blaine")
 
+Potluck.create(name: "This is the one!", location: "Best place ever!", date_at: "#{rand(10)}#{rand(10)}#{rand(10)}#{rand(10)}-#{rand(10)}#{rand(10)}-#{rand(10)}#{rand(10)}", time_at: "0#{rand(9)}:#{rand(5)}#{rand(9)}", user_id: 1)
+
 9.times do
   User.create(username: Faker::Pokemon.unique.name, email: Faker::Internet.unique.email, password: Faker::Internet.password)
 end
